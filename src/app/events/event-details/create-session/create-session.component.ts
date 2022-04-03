3035d95b75ad74bbc0a1e699196a8dddbec7b257
ioanbini <83530524+ioanbini@users.noreply.github.com>
2022-04-03 21:19:56 +0300

    create custom validator

--- src/app/events/event-details/create-session/create-session.component.ts
+++ src/app/events/event-details/create-session/create-session.component.ts
@@ -1,5 +1,6 @@
-import { Form, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { restrictedWords } from 'src/app/shared';
 import { iSessions } from '../../event.model';
 
 @Component({
@@ -8,37 +9,39 @@ import { iSessions } from '../../event.model';
   styleUrls: ['./create-session.component.scss']
 })
 export class CreateSessionComponent implements OnInit {
-  newSessionForm!:FormGroup
-  name!:FormControl
-  present!:FormControl;
-  duration!:FormControl;
-  level!:FormControl;
-  abstract!:FormControl
+  newSessionForm!: FormGroup
+  name!: FormControl
+  present!: FormControl;
+  duration!: FormControl;
+  level!: FormControl;
+  abstract!: FormControl
 
   constructor() {
 
   }
 
   ngOnInit(): void {
-    this.name = new FormControl('',Validators.required);
-    this.present = new FormControl('',Validators.required)
-    this.duration = new FormControl('',Validators.required)
-    this.level = new FormControl('',Validators.required);
-    this.abstract = new FormControl('',[Validators.required,Validators.maxLength(400)]) ;
+    this.name = new FormControl('', Validators.required);
+    this.present = new FormControl('', Validators.required)
+    this.duration = new FormControl('', Validators.required)
+    this.level = new FormControl('', Validators.required);
+    this.abstract = new FormControl('', [Validators.required, Validators.maxLength(20), restrictedWords(['example', 'foo', 'bar'])]);
 
-    this.newSessionForm= new FormGroup ({
-      name:this.name,
-      present:this.present,
-      duration:this.duration,
+    this.newSessionForm = new FormGroup({
+      name: this.name,
+      present: this.present,
+      duration: this.duration,
       level: this.level,
-      abstract: this .abstract
+      abstract: this.abstract
 
     })
 
   }
 
-  saveSession(formValue:any) {
-    let iSession :iSessions = {
+
+
+  saveSession(formValue: any) {
+    let iSession: iSessions = {
       id: undefined,
       name: formValue.name,
       presenter: formValue.present,
